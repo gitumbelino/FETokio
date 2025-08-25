39be355e1b11621fb5947c64865f1c36428f8342
gitumbelino <filipe_umbelino@proton.me>
2025-08-25 23:51:33 +0100

    exe api

--- Aulas Javascript/Other/6.ArraysAndObjects/API/API.js
+++ Aulas Javascript/Other/6.ArraysAndObjects/API/API.js
@@ -2,40 +2,62 @@ const form = document.querySelector("form");
 const todoList = document.querySelector(".todos");
 const apiUrl = `http://localhost:4000/todos`;
 
-
 const loadTodos = () => {
 
     todoList.innerHTML = ""
 
     fetch(apiUrl)
-    .then(response => response.json())
-    .then(todos => {
-        
-        todos.slice(0, 5).forEach(todo => {
+        .then(response => response.json())
+        .then(todos => {
 
-            const completed = todo.completed ? "completed" : "incompleted"
-            todoList.innerHTML += `<div class= "todo ${completed}">${todo.title}
-            </div>`  
-        })  
-    })
+            todos.forEach(todo => {
+
+                const completed = todo.completed ? "completed" : "incompleted"
+                const buttonText = todo.completed ? "Mark Complete" : "Mark Incomplete";
+                todoList.innerHTML += `
+                
+                <div user-id=${todo.userId} 
+                    todo-id=${todo.id} 
+                    class= "todo ${completed}">
+                    <div>${todo.title}</div>
+                <div><button onclick="toggleCompletion()">${buttonText}</button>
+                </div>
+                </div>
+                `;
+            })
+        })
 }
 
 
-
-document.querySelector("form").addEventListener("submit", async  (e) => {
-
-    e.preventDefault()  
-    const name = e.target.title.value
-    console.log(name)
+toggleCompletion = (e) => {
+    console.log(e)
+}
 
 
-    const response = await fetch("https://example.org/post", {
+window.onload = () => {
+    loadTodos()
+}
+
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+
+    e.preventDefault()
+
+    const title = e.target.title.value
+    console.log(title)
+
+
+    const response = await fetch(apiUrl, {
         method: "POST",
         headers: {
-          "Content-Type": "application/json",
+            "Content-Type": "application/json",
         },
-        body: JSON.stringify({ username: "example" }),
-        // …
-      });
+        body: JSON.stringify({ title, userId: "1", completed: false })
+    });
+
+    console.log(response)
+    loadTodos()
 
 })
+
+
