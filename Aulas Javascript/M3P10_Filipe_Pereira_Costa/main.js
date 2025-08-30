c10790815d438328a7abb7fe477e9f25429b71f8
gitumbelino <filipe_umbelino@proton.me>
2025-08-30 18:26:52 +0100

    loop

--- Aulas Javascript/M3P10_Filipe_Pereira_Costa/main.js
+++ Aulas Javascript/M3P10_Filipe_Pereira_Costa/main.js
@@ -1,20 +1,18 @@
 
 // 1. Criar a classe Persona com os atributos: nome, apelido e idade.
 // 2. Os objetos pessoais devem ser armazenados num array.
+//fiz ligeiramente diferente dos videos da tokio, porque já aprendi java e 
+//tentei aplicar os mesmos conceitos de oop que aprendi, com o objectivo de criar um objecto vazio e poder usá-lo
+// de forma repetida.
 
-const apiUrl = `http://localhost:4000/users`;
 class User {
-    constructor(name, surname, age, items 
-    ) {
+    constructor(name, surname, age, items) {
         this.name = name;
         this.surname = surname;
         this.age = age;
         this.items = items
     }
-
 }
-
-
 
 // 3. Criar no documento HTML um botão "Adicionar" que quando clicado chama a função "Persona()", e um botão "Mostrar" que
 // quando clicado exibe todas as pessoas armazenadas no array.
@@ -22,86 +20,67 @@ class User {
 // 4. Criar a função Persona(). Esta função irá pedir para introduzir um nome próprio, pedir para introduzir um apelido e pedir para
 // introduzir a idade.
 
-
+const apiUrl = `http://localhost:4000/users`;
 const button = document.getElementById("submit-btn");
 
 
-async function addUser(e){
+let counter = 0
 
-    e.preventDefault();
+async function addUser(e) {
+    // 5. A função deve repetir este procedimento cinco vezes.
 
-    const userNameInput = document.querySelector("#user-name");
-    const userName = userNameInput.value;
+    //adicionei um try catch para poder validar se objecto foi gravado com sucesso no array.
+    //desta forma posso confirmar cada tentativa
 
-    const userSurnameInput = document.querySelector("#user-surname");
-    const userSurname = userSurnameInput.value;
+    try {
 
-    const ageInput = document.querySelector("#user-age");
-    const age = ageInput.value;
+        e.preventDefault();
 
+        const userNameInput = document.querySelector("#user-name");
+        const userName = userNameInput.value;
 
-    const itemsInput = document.querySelector("#user-items");
-    const items = itemsInput.value.split(', ');
+        const userSurnameInput = document.querySelector("#user-surname");
+        const userSurname = userSurnameInput.value;
 
-
-    if (!userName || !userSurname || !age) return;
-
-    let newUser = new User(userName, userSurname, age, items);
-
-
-    await fetch(apiUrl, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(newUser)
-    });
+        const ageInput = document.querySelector("#user-age");
+        const age = ageInput.value;
 
 
+        const itemsInput = document.querySelector("#user-items");
+        const items = itemsInput.value.split(', ');
+
+
+        if (!userName || !userSurname || !age) return;
+
+        let newUser = new User(userName, userSurname, age, items);
+
+
+        await fetch(apiUrl, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newUser)
+        });
+
+        document.getElementById("new-user-form").reset();
+
+        counter++
+        return true;
+
+    } catch {
+        return false;
+    }
+
+}
+
+button.addEventListener("click", addUser);
+
+if (counter < 5) {
+    addUser()
+} else {
+    alert("you have successfully added 5 user profiles");
 }
 
 
 
-button.addEventListener("click", addUser);
-
-
-
-// button.addEventListener("click", async function (e) {
-
-//     e.preventDefault();
-
-//     const userNameInput = document.querySelector("#user-name");
-//     const userName = userNameInput.value;
-
-//     const userSurnameInput = document.querySelector("#user-surname");
-//     const userSurname = userSurnameInput.value;
-
-//     const ageInput = document.querySelector("#user-age");
-//     const age = ageInput.value;
-
-
-//     const itemsInput = document.querySelector("#user-items");
-//     const items = itemsInput.value.split(', ');
-
-
-//     if (!userName || !userSurname || !age) return;
-
-//     let newUser = new User(userName, userSurname, age, items);
-
-
-//     await fetch(apiUrl, {
-//         method: "POST",
-//         headers: {
-//             "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify(newUser)
-//     });
-
-// });
-
-
-
-
-
-
-// 5. A função deve repetir este procedimento cinco vezes.
