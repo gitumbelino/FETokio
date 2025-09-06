e744c7ae57c070ea53c75f5e61207e6cddcd7efa
gitumbelino <filipe_umbelino@proton.me>
2025-09-06 10:40:11 +0100

    formjsemailfield

--- AulasJavascript/M3P12_Filipe_Pereira_Costa/main.js
+++ AulasJavascript/M3P12_Filipe_Pereira_Costa/main.js
@@ -1,7 +1,6 @@
 // 3. Criar um script para validar o formulário:
-// • O script deve verificar se o nome do utilizador 
-//tem pelo menos 4 caracteres.
-// • O script deve verificar se o e-mail introduzido é válido. 
+
+
 
 //Para tal, utilizaremos uma expressão regular (RegEx Expression)
 // fornecida:
@@ -16,11 +15,72 @@
 
 const signUpForm = document.getElementById("signUp");
 
-const userLengthError = document.getElementById("userLengthError");
+
 const emailError = document.getElementById("emailError");
 const passwordError = document.getElementById("passwordError");
 const repeatPasswordError = document.getElementById("repeatPasswordError");
 
+
+const userNameInput = document.querySelector("#username");
+
+userNameField = () => {
+
+    const userName = userNameInput.value;
+
+    const userLengthError = document.getElementById("userLengthError");
+
+    // • O script deve verificar se o nome do utilizador 
+    //tem pelo menos 4 caracteres.
+    if (userName.length < 4) {
+        userLengthError.innerHTML = `
+        4 characters minumum
+`
+    } else {
+        userLengthError.innerHTML = `
+`
+    }
+}
+
+
+userNameInput.addEventListener("input", userNameField);
+
+
+
+
+
+
+emailField = () => {
+
+    const emailInput = document.querySelector("#email");
+    
+    const email = emailInput.value;
+    const emailError = document.getElementById("emailError");
+    const regex = `/^([a-zA-Z0-9_\ \-])+((([a-zA-Z0-9-])+(([a-zA-Z0-9]{2,4})+$/`;
+
+
+
+}
+
+
+
+
+// const form = document.querySelector("#form");
+// const input = document.querySelector("#phone");
+// const output = document.querySelector("#output");
+
+// const re = /^(?:\d{3}|\(\d{3}\))([-/.])\d{3}\1\d{4}$/;
+
+// function testInfo(phoneInput) {
+//     const ok = re.exec(phoneInput.value);
+
+//     output.textContent = ok
+//         ? `Thanks, your phone number is ${ok[0]}`
+//         : `${phoneInput.value} isn't a phone number with area code!`;
+
+// form.addEventListener("submit", (event) => {
+//     event.preventDefault();
+//     testInfo(input);
+// });
 
 
 
@@ -30,12 +90,11 @@ signUpForm.addEventListener("submit", function (e) {
 
     e.preventDefault();
 
-    const userNameInput = document.querySelector("#username");
-    const userName = userNameInput.value;
+
+    userNameField();
 
 
-    const emailInput = document.querySelector("#email");
-    const email = emailInput.value;
+
 
     const passwordInput = document.querySelector("#password");
     const password = passwordInput.value;
@@ -43,11 +102,6 @@ signUpForm.addEventListener("submit", function (e) {
     const repeatPasswordInput = document.querySelector("#repeatPassword");
     const repeatPassword = repeatPasswordInput.value;
 
-    if (userName.length < 4) {
-        userLengthError.innerHTML = `
-        4 characters minumum
-`
-    }
 
 
 
