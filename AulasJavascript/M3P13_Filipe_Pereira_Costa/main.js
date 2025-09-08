8ebfe0ad33d157f6fb09f6909be9a13e7289e8fb
gitumbelino <filipe_umbelino@proton.me>
2025-09-08 23:57:18 +0100

    P

--- AulasJavascript/M3P13_Filipe_Pereira_Costa/main.js
+++ AulasJavascript/M3P13_Filipe_Pereira_Costa/main.js
@@ -5,36 +5,79 @@
 
 const linkElements = document.querySelectorAll('a[href]');
 
-// for(let i = 0; i < linkElements.length; i++ ){
-
-//     console.log(linkElements[i].href);
-// }
-
-
 const showLinksButton = document.getElementById("nrLinks");
 const linksText = document.querySelector("#nrLinksButton");
-
-
 
 showLinks = () => {
 
     nrLinks.innerHTML = `
-     There are ${linkElements.length} <br>
-     links in this webpage.     
-    `
+        There are ${linkElements.length} <br>
+        links in this webpage.     
+        `
 }
 
-
-
-
-
-
-nrLinksButton.addEventListener("click", showLinks);
-
-
+showLinksButton.addEventListener("click", showLinks);
 
 
 
 // o O endereço para o qual o penúltimo link está ligado.
-// o O número de links que nos direcionam para https://twitter.com
+
+const urlAddressText = document.querySelector("#linkAddressUrl");
+const linkAddressNameButton = document.getElementById("linkAddressNameButton")
+
+linkAddress = () => {
+
+    let position = linkElements.length - 2;
+
+    urlAddressText.innerHTML = `
+        The link is: <br> ${linkElements[position].href} 
+            
+        `
+}
+
+linkAddressNameButton.addEventListener("click", linkAddress);
+
+
+
+const twitterLinkText = document.querySelector("#twitterLinks");
+const twitterLinksButton = document.getElementById("twitterLinksButton")
+
+twitter = () => {
+
+    let counter = 0;
+
+    for (let i = 0; i < linkElements.length; i++) {
+
+        if (linkElements[i].href.includes("twitter"))
+            counter++;
+
+    }
+
+    twitterLinkText.innerHTML = `
+        Nr of Twitter links: <br>  ${counter} 
+        `
+}
+
+twitterLinksButton.addEventListener("click", twitter);
+
+
+
 // o O número de links contidos no terceiro parágrafo. 
+
+const paragraphLinksText = document.getElementById("paragraphLinks");
+const paragraphLinksButton = document.getElementById("paragraphLinksButton");
+
+const linkElementsThirdP = document.querySelectorAll('#información :nth-child(1)');
+
+paragraph = () => {
+
+    paragraphLinksText.innerHTML = `
+        The  paragraph contains ${linkElementsThirdP.length} links. 
+        `
+}
+
+console.log(linkElementsThirdP)
+
+
+
+paragraphLinksButton.addEventListener("click", paragraph);
